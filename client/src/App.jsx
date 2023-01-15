c4158eb205fcdaf6e1488069e46a47d93a8c3354
codelande <co.delande@yahoo.fr>
2023-01-15 14:48:20 +0100

    finish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./assets/css/weather-icons.min.css";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState({});
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  useEffect(() => {
+    fetch("http://localhost:3000/api/daily")
+      .then((res) => res.json())
+      .then((data) => setWeather(data));
+    console.log(weather);
+  }, []);
+
+  if (weather.time) {
+    return (
+      <Grid container spacing={2} m='80px'>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent sx={{ display: "flex", flexDirection: "column"}}>
+            <Typography
+              sx={{ fontSize: 28 }}
+              color="text.secondary"
+              gutterBottom
+              fontWeight={"bold"}
+            >
+              <CalendarMonthIcon fontSize="large" /> {weather.time[0]}
+            </Typography>
+            <Typography pt={'20px'} variant="h5" component="div" fontSize={"8rem"} alignSelf={'center'}>
+              <i className={`wi wi-wmo4680-${weather.weathercode[0]}`}></i>
+            </Typography>
+            <Typography pt={'50px'} sx={{ mb: 1.5 }} color="blue" alignSelf={'center'}>
+              Minimal temperature: {weather.temperature_2m_min[0]}°C
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="orange" alignSelf={'center'}>
+              Maximal temperature: {weather.temperature_2m_max[0]}°C
+            </Typography>
+          </CardContent>
+        </Card>
+        <Box flex={"column"} ml={"8px"}>
+          {weather.time.map(
+            (day, index) =>
+              index > 1 &&
+              index <= 4 && (
+                <Box key={index} my={"8px"}>
+                  <Card>
+                    <CardContent sx={{ display: "flex", flexDirection: "row" }}>
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 14 }}
+                          color="text.secondary"
+                          gutterBottom
+                          fontWeight={"bold"}
+                        >
+                          <CalendarMonthIcon /> {weather.time[index]}
+                        </Typography>
+                        <Typography pt={"8px"} color="blue">
+                          {weather.temperature_2m_min[index]}°C
+                          <Typography color="orange">
+                            {weather.temperature_2m_max[index]}°C
+                          </Typography>
+                        </Typography>
+                      </Box>
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        alignSelf={"center"}
+                        pl={"25px"}
+                        fontSize={"3rem"}
+                      >
+                        <i
+                          className={`wi wi-wmo4680-${weather.weathercode[index]}`}
+                        ></i>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Box>
+              )
+          )}
+        </Box>
+      </Grid>
+    );
+  }
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/daily", async (req, res) => {
+  async function getData() {
+    const response = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=47.90&longitude=1.90&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FBerlin"
+    );
+
+    return response.json();
+  }
+  const meteoData = await getData();
+  const dailyData = meteoData.daily;
+  res.send(dailyData);
 });
 
 app.listen(port, () => {
